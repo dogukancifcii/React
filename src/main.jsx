11972ada6962fb7148fd24af2baf7a370cfa2083
Dogukan Cifci <dogukancifcii5@gmail.com>
2025-03-08 16:33:16 +0300

    app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client"; //default export olmadigi icin parantez icinde nprmal export oldugu icin
+import App from "./App"; //default export oldugu icin parantez icinde degil
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
